0abcde134368e8b47b242bb0da3148566f2c2a1f
Thiago Almeida <thiagoa.silva@outlook.com>
2020-06-27 16:06:25 -0300

    configured ui kit provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
+import * as eva from '@eva-design/eva';
 
 import Router from './routes';
 import {persistor, store} from './store';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 
 const App: React.FC<{}> = ({}) => {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <Router />
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <IconRegistry icons={EvaIconsPack} />
+          <Router />
+        </ApplicationProvider>
       </PersistGate>
     </Provider>
   );
